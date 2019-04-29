0d2897840b562b4c86505d3293b357fc12f1e891
Nicole Cooper-White <nwhite1018@gmail.com>
2019-04-29 10:40:12 -0600

    gallery updates

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable require-jsdoc */
 /* eslint-disable linebreak-style */
 /* eslint-disable eol-last */
 /* eslint-disable max-len */
@@ -80,4 +81,56 @@ $(document).ready(function() {
       }
     });
   });
+
+  // Portfolio
+
+  // carousel settings
+  $(function() {
+    $('#mdb-lightbox-ui').load('mdb-addons/mdb-lightbox-ui.html');
+  });
+
+  $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
+
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  $('#left-chevron').on('click', function() {
+    console.log('back clicked');
+    // showSlides(slideIndex --);
+  });
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    // let i;
+    // const slides = document.getElementsByClassName('mySlides');
+    // const dots = document.getElementsByClassName('demo');
+    // const captionText = document.getElementById('caption');
+    // if (n > slides.length) {
+    //   slideIndex = 1;
+    // }
+    // if (n < 1) {
+    //   slideIndex = slides.length;
+    // }
+    // for (i = 0; i < slides.length; i++) {
+    //   slides[i].style.display = 'none';
+    // }
+    // for (i = 0; i < dots.length; i++) {
+    //   dots[i].className = dots[i].className.replace(' active', '');
+    // }
+    // slides[slideIndex - 1].style.display = 'block';
+    // dots[slideIndex - 1].className += ' active';
+    // captionText.innerHTML = dots[slideIndex - 1].alt;
+  }
 });
